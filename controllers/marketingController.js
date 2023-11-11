47623d88804bb32b142e5e08e00cb30a10d3a7cb
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-11-11 09:26:32 +0600

    get a single data using by id

--- controllers/marketingController.js
+++ controllers/marketingController.js
@@ -3,7 +3,12 @@ const getAllData = async (req, res) => {
   const result = await marketingData.find();
   res.send(result);
 };
-const getSingleData = async (req, res) => {};
+const getSingleData = async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: id };
+  const result = await marketingData.findById(query);
+  res.send(result);
+};
 const createAData = async (req, res) => {
   const data = req.body;
   const result = await marketingData.create(data);
--- routes/marketingRoute.js
+++ routes/marketingRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createAData,
   getAllData,
+  getSingleData,
 } = require("../controllers/marketingController");
 const marketingRoute = express.Router();
 
 // marketing Route
 marketingRoute.get("/", getAllData);
+marketingRoute.get("/:id", getSingleData);
 marketingRoute.post("/", createAData);
 
 module.exports = marketingRoute;
